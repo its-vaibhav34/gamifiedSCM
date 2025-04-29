ee66486ea3959fde378675eee3d3e5a90d010375
adityasharma0903 <adityasharma.5672@gmail.com>
2025-04-29 12:55:53 +0530

    added

--- git.js
+++ git.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await fetch(`https://api.github.com/users/${username}`, {
             method: 'GET',
             headers: {
-              'Authorization': 'Bearer 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',
+              'Authorization': 'Bearer ',
               'Accept': 'application/vnd.github+json'
             }
           })
