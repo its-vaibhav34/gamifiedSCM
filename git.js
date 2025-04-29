0d53407c8db8afed441e92f11cd16fdcb0ac883c
vaibhav34 <vaibhav13467@gmail.com>
2025-04-29 14:17:41 +0530

    ignore

--- git.js
+++ git.js
@@ -60,15 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await fetch(`https://api.github.com/users/${username}`, {
             method: 'GET',
             headers: {
-              'Authorization': 'Bearer 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',
-              'Accept': 'application/vnd.github+json'
+              'Authorization': 'Bearer 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'
             }
           })
             
         if (!response.ok) {
           throw new Error("User not found")
         }
-  
+
         const userData = await response.json()
         currentUser = userData
   
