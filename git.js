e98f09903605521ae05b5719bca1bb3efeb6772e
adityasharma0903 <adityasharma.5672@gmail.com>
2025-04-29 13:39:51 +0530

    added

--- git.js
+++ git.js
@@ -514,7 +514,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     try {
       // Fetch friend's repositories
-      const response = await fetch(`https://api.github.com/users/${friend.login}/repos?sort=updated&per_page=100`);
+      // const response = await fetch(`https://api.github.com/users/${friend.login}/repos?sort=updated&per_page=100`);
       
       if (!response.ok) {
         if (response.status === 403) {
