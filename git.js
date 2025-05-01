0264ef6f1c3edc811ea6e14201241bfee693c055
Abhi-git-dev1 <yadavabhi12431@gmail.com>
2025-05-01 15:33:19 +0530

    New added

--- git.js
+++ git.js
@@ -206,8 +206,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const response = await fetch(`https://api.github.com/users/${username}`, {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer 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',
-          'Accept': 'application/vnd.github+json'
+          'Authorization': 'Bearer '
         }
       });
       
